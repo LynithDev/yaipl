b72dbb4a632dd3fcf2b11d3ffb0a5f1fe7450e3a
LynithDev <61880709+LynithDev@users.noreply.github.com>
2024-02-24 12:07:03 +0100

    feat: REPL

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,21 @@
-use std::{fs, process::exit};
+use std::{fs, io::{stdin, stdout, Read, Write}, process::exit};
 
-use another_interpreted_language::{errors::ErrorList, extract_type, lexer::Lexer, parser::{Parser, ParserErrors, TokenMismatch}, utils::{BLUE, BOLD, MAGENTA, RED, RESET, UNDERLINE}};
+use another_interpreted_language::{errors::ErrorList, extract_type, lexer::Lexer, parser::{Parser, ParserErrors, TokenMismatch}, utils::{BLUE, BOLD, MAGENTA, RED, RESET}};
+
+pub const NAME: &str = "YAIPL";
+pub const NAME_LONG: &str = "Yet Another Interpreted Programming Language";
+pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
 
     if args.len() < 2 {
-        let binary: String = if args.len() < 1 {
-            String::from("yaipl")
-        } else {
-            args[0].to_owned()
-        };
-
-        println!("Missing input file");
-        println!("USAGE: {} <file>", binary);
+        // No input file provided, run REPL
+        repl();
         exit(1);
     }
 
+    // Input file provided
     let file_path = &args[1];
     let cwd = std::env::current_dir().expect("Could not get current directory");
     let absolute_path = match fs::canonicalize(file_path) {
@@ -33,26 +32,32 @@ fn main() {
     };
 
     if let Err(err) = parse_file(file_path) {
-        extract_type!(err, ParserErrors, TokenMismatch, (mismatch) => {
-            let path = format!("{}:{}:{}", pretty_path, mismatch.position.line, mismatch.position.col);
-            println!("{}{}{} error{} at '{}{}{}'", RED, BOLD, mismatch.get_name(), RESET, BLUE, path, RESET);
-
-            print!("->{}{} ", MAGENTA, BOLD);
-            if mismatch.expected.len() > 1 {
-                println!("Expected tokens of type {:?} but found '{:?}'", mismatch.expected, mismatch.found);
-            } else {
-                println!("Expected token of type '{:?}' but found '{:?}'", mismatch.expected[0], mismatch.found);
-            }
-            print!("{}", RESET);
-
-            return;
-        });
-
-        println!("{:#?}", err);
+        handle_errors(err, Some(pretty_path));
     }
 }
 
-fn parse_file(path: &String) -> Result<(), Box<dyn ErrorList>> {
+pub fn repl() -> Result<(), Box<dyn ErrorList>> {
+    println!("{} - REPL Mode", NAME);
+    
+    let stdin = stdin();
+    let mut buf = String::new();
+
+    loop {
+        print!("{}{}>>>{} ", BOLD, BLUE, RESET);
+        let _ = stdout().flush();
+        let _ = stdin.read_line(&mut buf);
+
+        let mut lexer = Lexer::from(&buf);
+        let tokens = lexer.tokenize()?;
+
+        let mut parser = Parser::from(&tokens);
+        let ast = parser.parse()?;
+
+        println!("{:#?}", ast);
+    }
+}
+
+pub fn parse_file(path: &String) -> Result<(), Box<dyn ErrorList>> {
     let content = match fs::read_to_string(path) {
         Ok(text) => text,
         Err(err) => {
@@ -70,5 +75,28 @@ fn parse_file(path: &String) -> Result<(), Box<dyn ErrorList>> {
     println!("\n----\nProgram\n{:#?}\nProgram\n----", ast);
 
     Ok(())
+}
+
+fn handle_errors(err: Box<dyn ErrorList>, path: Option<String>) {
+    extract_type!(err, ParserErrors, TokenMismatch, (mismatch) => {
+        let path = match path {
+            Some(path) => format!("{}:{}:{}", path, mismatch.position.line, mismatch.position.col),
+            None => format!("{}:{}", mismatch.position.line, mismatch.position.col)
+        };
+
+        println!("{}{}{} error{} at '{}{}{}'", RED, BOLD, mismatch.get_name(), RESET, BLUE, path, RESET);
+
+        print!("->{}{} ", MAGENTA, BOLD);
+        if mismatch.expected.len() > 1 {
+            println!("Expected tokens of type {:?} but found '{:?}'", mismatch.expected, mismatch.found);
+        } else {
+            println!("Expected token of type '{:?}' but found '{:?}'", mismatch.expected[0], mismatch.found);
+        }
+        print!("{}", RESET);
+
+        return;
+    });
+
+    println!("{:#?}", err);
 }
 
