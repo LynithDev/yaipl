46f73b387fd6028cfcfb11177ea0467f54ba2e0a
LynithDev <61880709+LynithDev@users.noreply.github.com>
2024-02-29 19:15:40 +0100

    statements

--- src/evaluator/mod.rs
+++ src/evaluator/mod.rs
@@ -1,7 +1,7 @@
 
 use std::error::Error;
 
-use crate::{create_error_list, error, parser::ast::{ArithmeticOperator, Assignment, BinaryExpression, BlockStatement, Expression, FunctionCallExpression, FunctionDeclareExpression, Identifier, Literal, LogicalOperator, Node, Operator, UnaryExpression}};
+use crate::{create_error_list, error, parser::ast::{ArithmeticOperator, Assignment, BinaryExpression, BlockStatement, Expression, FunctionCallExpression, FunctionDeclareExpression, Identifier, IfStatement, Literal, LogicalOperator, Node, Operator, ReturnStatement, UnaryExpression}};
 
 use self::{environment::Environment, object::{Object, ObjectType}};
 
@@ -11,6 +11,7 @@ pub mod yaipl_std;
 
 create_error_list!(EvaluatorErrors, {});
 pub type EvaluatorResult<T> = Result<T, Box<dyn Error>>;
+pub type StatementResult<T> = EvaluatorResult<(T, bool)>;
 
 pub struct Evaluator<'a> {
     env: Environment<'a>,
@@ -33,28 +34,57 @@ impl<'a> Evaluator<'a> {
     }
 
     pub fn eval(&'a mut self) -> Result<Object, EvaluatorErrors> {
-        let mut result: Option<Object> = None;
+        let mut result: Option<(Object, bool)> = None;
         
         for node in self.ast {
             result = Some(self.eval_statement(node)?);
         }
 
-        Ok(result.unwrap_or(Object::void()))
+        Ok(result.unwrap_or((Object::void(), false)).0)
     } 
 
-    fn eval_statement(&mut self, node: &'a Node) -> EvaluatorResult<Object> {
+    fn eval_statement(&mut self, node: &'a Node) -> StatementResult<Object> {
         match node {
-            Node::EmptyStatement(_) => Ok(Object::void()),
-            Node::ExpressionStatement(expr) => self.eval_expression(&expr.0),
-            _ => error!(format!("Not implemented statement {:?}", node))
+            Node::BlockStatement(block) => self.eval_block(block),
+            Node::BreakStatement(_) => Ok((Object::void(), true)),
+            Node::ContinueStatement(_) => Ok((Object::void(), true)),
+            Node::EmptyStatement(_) => Ok((Object::void(), false)),
+            Node::ExpressionStatement(expr) => Ok((self.eval_expression(&expr.0)?, false)),
+            Node::IfStatement(statement) => self.eval_if(statement),
+            Node::ReturnStatement(statement) => self.eval_return(statement),
+            _ => error!(format!("Not implemented statement {:#?}", node))
         }
+    }
+
+    fn eval_return(&mut self, statement: &'a ReturnStatement) -> StatementResult<Object> {
+        let ReturnStatement(expression) = statement;
+        let mut result = Object::void();
+
+        if let Some(expression) = expression {
+            result = self.eval_expression(expression)?;
+        }
+
+        Ok((result, true))
+    }
+
+    fn eval_if(&mut self, statement: &'a IfStatement) -> StatementResult<Object> {
+        let IfStatement(condition, block) = statement;
+        let condition = self.eval_expression(condition)?;
+
+        if condition.is(ObjectType::Boolean) {
+            if condition.as_boolean().expect("Couldn't take as boolean") {
+                return self.eval_block(block);
+            }
+        }
+
+        Ok((Object::void(), false))
     }
 
     fn eval_expression(&mut self, expression: &'a Expression) -> EvaluatorResult<Object> {
         Ok(match expression {
             Expression::AssignmentExpr(expression) => self.eval_assignment_expression(expression)?,
             Expression::BinaryExpr(expression) => self.eval_binary_expression(expression)?,
-            Expression::BlockExpr(expression) => self.eval_block(expression)?,
+            Expression::BlockExpr(expression) => (self.eval_block(expression)?).0,
             Expression::FunctionCallExpr(expression) => self.eval_func_call_expression(expression)?,
             Expression::FunctionDeclareExpr(expression) => self.eval_func_declare_expression(expression)?,
             Expression::GroupExpr(expression) => self.eval_expression(expression)?,
@@ -104,22 +134,25 @@ impl<'a> Evaluator<'a> {
                         built_args.push(self.eval_expression(arg)?);
                     }
                     
-                    (function.2)(built_args)
+                    ((function.2)(built_args), false)
                 },
                 _ => error!("not function :(")
             };
 
-            return Ok(result);
+            return Ok(result.0);
         }
         
         error!("Couldn't find fucntion")
     }
 
-    fn eval_block(&mut self, expression: &'a BlockStatement) -> EvaluatorResult<Object> {
-        let mut result = Object::void();
+    fn eval_block(&mut self, expression: &'a BlockStatement) -> StatementResult<Object> {
+        let mut result = (Object::void(), false);
         
         for statement in &expression.0 {
             result = self.eval_statement(statement)?;
+            if result.1 {
+                break;
+            }
         }
 
         Ok(result)
